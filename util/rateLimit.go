d599b1f89f19fddcd636a3abdf43c8c4ac9510d5
cherry.chen <cherrycmd@gmail.com>
2019-04-12 11:01:08 +0800

    modify the rate

--- util/rateLimit.go
+++ util/rateLimit.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	gredis "github.com/go-redis/redis"
 	"shorturl/middleware/redis"
 	"strconv"
 	"time"
@@ -9,27 +10,27 @@ import (
 func (u *Util) Rate(key string, num, gap int) bool {
 	rateLimitKey := "rate_limit_" + key
 	expires := gap * 2
-	lens, _ := redis.Redis().LLen(rateLimitKey).Result()
 	nTime := time.Now()
 	now := nTime.Unix()
-	if lens < int64(num) {
-		pipe := redis.Redis().Pipeline()
-		redis.Redis().LPush(rateLimitKey, now)
-		pipe.Expire(rateLimitKey, time.Duration(expires)*time.Second)
-		pipe.Exec()
+	pipe := redis.Redis().Pipeline()
+	pipe.LPush(rateLimitKey, now)
+	pipe.Expire(rateLimitKey, time.Duration(expires)*time.Second)
+	pipes, err := pipe.Exec()
+	if err != nil {
 		return false
-	} else {
-		redisNow := redis.Redis().LIndex(rateLimitKey, -1)
+	}
+	len := pipes[0].(*gredis.IntCmd).Val()
+	if len > int64(num) {
+		redisNow := redis.Redis().LIndex(rateLimitKey, int64(num-1))
 		redisIntNow, _ := strconv.Atoi(redisNow.Val())
 		if (now - int64(redisIntNow)) < int64(gap) {
+			redis.Redis().LTrim(rateLimitKey, int64(-num), int64(-1))
 			return true
 		} else {
-			pipe := redis.Redis().Pipeline()
-			redis.Redis().LPush(rateLimitKey, now)
 			redis.Redis().LTrim(rateLimitKey, 0, int64(num-1))
-			pipe.Expire(rateLimitKey, time.Duration(expires)*time.Second)
-			pipe.Exec()
 			return false
 		}
+
 	}
+	return false
 }
